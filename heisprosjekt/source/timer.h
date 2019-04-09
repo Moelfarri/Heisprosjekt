b35ab5b72b28ac4203e17bf6f2cd214c628654b0
Moelfarri <47497819+Moelfarri@users.noreply.github.com>
2019-04-09 17:37:58 +0200

    Update timer.h

--- heisprosjekt/source/timer.h
+++ heisprosjekt/source/timer.h
@@ -1,27 +1,20 @@
 #pragma once
 
 
-/*Global time variable that
-keeps track of how many seconds have elapsed*/
-
-
-
-
 /*begins the timer by making
 timeElapsed take in time(NULL) from time.h library*/
-
 void time_begin_timer();
 
 
 /*we do not wish to freeze other processes
 and use therefore binary logic to element the
 3 second rule to the elevator */
-
 int time_is_up();
 
-/* sets timeElapsed back
+/* sets time_elapsed back
 to default state which is 0*/
-
 void time_end_timer();
 
+/*Get time_elapsed for
+external modules*/
 int get_time_elapsed();

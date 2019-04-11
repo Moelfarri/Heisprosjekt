6e1c6e657617f46008908a1934c32b7e0933c315
Moelfarri <47497819+Moelfarri@users.noreply.github.com>
2019-04-11 15:32:13 +0200

    Update timer.h

--- heisprosjekt/source/timer.h
+++ heisprosjekt/source/timer.h
@@ -2,13 +2,13 @@
 
 
 /*begins the timer by making
-timeElapsed take in time(NULL) from time.h library*/
+time_elapsed take in time(NULL) from time.h library and start counting*/
 void time_begin_timer();
 
 
 /*we do not wish to freeze other processes
 and use therefore binary logic to element the
-3 second rule to the elevator */
+3 second rule to the elevator. Returns 1 if time_Elapsed = 3 */
 int time_is_up();
 
 /* sets time_elapsed back
